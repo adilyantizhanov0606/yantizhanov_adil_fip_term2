ee4c80194674af749287f1cecc6f993a9ff7eee0
Adil <a_yantizhanov250550@fanshaweonline.ca>
2026-04-14 00:33:55 -0400

    Regular Vodka Page.
    Regular vodka page responsive layout was implemented as long as js functionality

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ console.log("fired");
 //variables
 const button = document.querySelector("#button");
 const burgerCon = document.querySelector("#menu-con");
+let features = {
+    design: "Encased in a sleek frosted-glass bottle with a minimalist, premium design aesthetic.",
+    glass: "Crafted using a refined multi-stage distillation process for exceptional smoothness and purity.",
+    flavour: "Made with high-quality ingredients to deliver a clean, crisp, and refined taste."
+},
+featuresCircles = document.querySelectorAll(".circle"),
+featuresBox = document.querySelector(".features-box")
 
 //functions
 function hamburgerMenu() {
@@ -10,5 +17,20 @@ function hamburgerMenu() {
 	button.classList.toggle("expanded");
 }
 
+function renderText (event) {
+    featuresBox.innerHTML = "";
+
+    let featureHeading = event.target.dataset.info;
+    let featureText = features[featureHeading];
+
+    let renderedText = document.createElement("p");
+    renderedText.textContent = featureText;
+    renderedText.classList.add("p-main");
+    featuresBox.appendChild(renderedText);
+}
+
 //Event Listeners
 button.addEventListener("click", hamburgerMenu);
+featuresCircles.forEach(function(circle) {
+    circle.addEventListener("click", renderText)
+});
